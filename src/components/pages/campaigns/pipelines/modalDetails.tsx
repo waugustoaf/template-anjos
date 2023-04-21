4b13f96e90528f8615d6dba718fabf33888fa591
Augusto Fernandes <waltheraaf@gmail.com>
2023-04-21 13:13:58 -0300

    style: fix pipelines campaigns resposivity

--- src/components/pages/campaigns/pipelines/modalDetails.tsx
+++ src/components/pages/campaigns/pipelines/modalDetails.tsx
@@ -1,16 +1,16 @@
-import {apiServices} from '@/services';
-import {GetCustomerCBResponse} from '@/services/customer/types';
-import {Button, Link, Typography} from '@mui/material';
-import {Box} from '@mui/system';
-import {useEffect, useState} from 'react';
-import {useForm} from 'react-hook-form';
-import {toast} from 'react-hot-toast';
-import {Icon} from "@/components/icon";
-import {SendActionMessage} from "@/components/pages/campaigns/pipelines/tabs/message";
-import {SendActionConversation} from "@/components/pages/campaigns/pipelines/tabs/conversation";
-import {SendActionSchedule} from "@/components/pages/campaigns/pipelines/tabs/schedule";
-import {SendActionSale} from "@/components/pages/campaigns/pipelines/tabs/sale";
-import {SendActionAppointment} from "@/components/pages/campaigns/pipelines/tabs/appointment";
+import { apiServices } from '@/services';
+import { GetCustomerCBResponse } from '@/services/customer/types';
+import { Button, Link, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Icon } from '@/components/icon';
+import { SendActionMessage } from '@/components/pages/campaigns/pipelines/tabs/message';
+import { SendActionConversation } from '@/components/pages/campaigns/pipelines/tabs/conversation';
+import { SendActionSchedule } from '@/components/pages/campaigns/pipelines/tabs/schedule';
+import { SendActionSale } from '@/components/pages/campaigns/pipelines/tabs/sale';
+import { SendActionAppointment } from '@/components/pages/campaigns/pipelines/tabs/appointment';
 
 interface PipelineCustomerMessageProps {
   customer: GetCustomerCBResponse['message'][0];
@@ -183,25 +183,38 @@ export function PipelineCustomerActions({
       </Typography>
 
       <Typography>
-        <Box gap={'0.9rem'} alignContent={'center'} alignItems={'center'} >
+        <Box gap={'0.9rem'} alignContent={'center'} alignItems={'center'}>
           {customer.name}
 
-          { customer.whatsApp &&
-            <Link href={'https://whatsa.me/55'+customer.whatsApp} target='_blank'>
-              &nbsp; &nbsp; &nbsp;<Icon icon={'tabler:brand-whatsapp'} fontSize={25} />
+          {customer.whatsApp && (
+            <Link
+              href={'https://whatsa.me/55' + customer.whatsApp}
+              target='_blank'
+            >
+              &nbsp; &nbsp; &nbsp;
+              <Icon icon={'tabler:brand-whatsapp'} fontSize={25} />
             </Link>
-          }
+          )}
 
-          { customer.instagram &&
-          <Link href={'https://instagram.com/'+customer.instagram} target='_blank'>
-            &nbsp;  <Icon icon={'tabler:brand-instagram'} fontSize={25} />
-          </Link>
-          }
+          {customer.instagram && (
+            <Link
+              href={'https://instagram.com/' + customer.instagram}
+              target='_blank'
+            >
+              &nbsp; <Icon icon={'tabler:brand-instagram'} fontSize={25} />
+            </Link>
+          )}
         </Box>
       </Typography>
 
-
-      <Box display='flex' alignItems='center' gap='0.5rem' mb='1rem'>
+      <Box
+        display='flex'
+        flexDirection={{ xs: 'column', md: 'row'}}
+        alignItems='center'
+        gap='0.5rem'
+        mb='1rem'
+        width='100%'
+      >
         <TabButton
           activeTab={currentTab}
           icon='tabler:message'
@@ -276,109 +289,6 @@ export function PipelineCustomerActions({
           onClose={onClose}
         />
       )}
-
-
-      {/*<Box
-        display='flex'
-        alignItems='center'
-        gap='1rem'
-        width='100%'
-        justifyContent='space-between'
-        flexDirection={{ sx: 'column', md: 'column', lg: 'row' }}
-        marginTop='1rem'
-      >
-        <Button
-          variant='contained'
-          sx={{
-            whiteSpace: 'nowrap',
-            textTransform: 'capitalize',
-            width: '100%',
-            minWidth: 'fit-content',
-          }}
-        >
-          Msg. Rede Social
-        </Button>
-        <Button
-          variant='text'
-          sx={{
-            whiteSpace: 'nowrap',
-            textTransform: 'capitalize',
-            width: '100%',
-            minWidth: 'fit-content',
-          }}
-        >
-          Msg. Conversa
-        </Button>
-        <Button
-          variant='text'
-          sx={{
-            whiteSpace: 'nowrap',
-            textTransform: 'capitalize',
-            width: '100%',
-            minWidth: 'fit-content',
-          }}
-        >
-          Consulta
-        </Button>
-        <Button
-          variant='text'
-          sx={{
-            whiteSpace: 'nowrap',
-            textTransform: 'capitalize',
-            width: '100%',
-            minWidth: 'fit-content',
-          }}
-        >
-          Agendamento
-        </Button>
-        <Button
-          variant='text'
-          sx={{
-            whiteSpace: 'nowrap',
-            textTransform: 'capitalize',
-            width: '100%',
-            minWidth: 'fit-content',
-          }}
-        >
-          Venda
-        </Button>
-      </Box>
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        style={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Box marginTop='1rem' width='100%'>
-          {mountForm({
-            fields: [
-              {
-                name: 'message',
-                rowSize: 12,
-                title: 'Mensagem',
-                type: 'textarea',
-                placeholder: 'Mensagem que foi enviada',
-              },
-            ],
-            errors,
-            defaultValues,
-            register,
-            setValue,
-            trigger,
-          })}
-        </Box>
-
-        <Box display='flex' alignItems='center' gap='1rem'>
-          <SubmitButton isLoading={isLoading} title='Salvar' />
-          <Button variant='text' onClick={onClose} sx={{ marginTop: '32px' }}>
-            Cancelar
-          </Button>
-        </Box>
-      </form>*/}
     </Box>
   );
 }
